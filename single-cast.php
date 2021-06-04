96d57e4044a80614230bb953440721dcef3eab55
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-05 03:05:43 +0900

    single-cast.phpの作成

--- single-cast.php
+++ single-cast.php
@@ -2,143 +2,93 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lady merry</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
-        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="shortcut icon" href="/img/favicon.ico">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="/js/wow.min.js"></script>
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/script.js"></script>
+    <?php get_header(); ?>
 </head>
 
 <body>
-    <header class="header">
-        <div class="header__inner inner">
-            <h1 class="header__logo"><a href="/">Lady merry</a></h1>
-            <!-- <ul class="header__nav">
-                <li><a href="" class="is-active">トップページ</a></li>
-                <li><a href="">お知らせ</a></li>
-                <li><a href="">キャスト一覧</a></li>
-                <li><a href="">店内写真</a></li>
-                <li><a href="">オンラインガールズバー</a></li>
-                <li><a href="">ログイン</a></li>
-            </ul> -->
-            <div class="drawer">
-                <input type="checkbox" class="drawer__checkbox" id="drawerCheckbox">
-                <label for="drawerCheckbox" class="drawer__icon">
-                    <span class="drawer__icon-parts"></span><!-- /.drawer__icon-parts -->
-                </label><!-- /.drawer__icon -->
-                <label for="drawerChechbox" class="drawer__overlay"></label><!-- /.drawer__overlay -->
-                <nav class="drawer__menu">
-                    <ul>
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">トップページ</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">お知らせ</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">キャスト</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">店舗情報</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">オンラインガールズバー</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">ログイン</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                    </ul>
-                </nav><!-- /.drawer__menu -->
-            </div><!-- /.drawer -->
-        </div>
-    </header><!-- /.header -->
+    <?php get_template_part('includes/header-menu'); ?>
     <section class="cast-single">
         <div class="cast-single__inner inner wow fadeIn" data-wow-duration="4s">
             <div class="cast">
                 <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-                <h1 class="cast__head">来未</h1><!-- /.cast__head -->
+                <h1 class="cast__head"><?php the_title(); ?></h1><!-- /.cast__head -->
                 <div class="SPACER--60"></div><!-- /.SPACER--60 -->
                 <div class="cast__img">
-                    <img src="/img/cast1.png" alt="">
+                    <?php if (has_post_thumbnail()) :
+                        $id = get_post_thumbnail_id();
+                        $img = wp_get_attachment_image_src($id, 'large');
+                    else :
+                        $img = array(get_template_directory_uri() . '/img/noimg.png');
+                    endif;
+                    ?>
+                    <?php echo '<img src="' . esc_url($img[0]) . '">'; ?>
                 </div><!-- /.cast__img -->
                 <div class="SPACER--60"></div><!-- /.SPACER--60 -->
                 <div class="cast__table">
                     <table class="cell">
-                        <tr>
-                            <th>名前</th>
-                        </tr>
-                        <tr>
-                            <td>来未</td>
-                        </tr>
+                        <?php if (get_field('name')) : ?>
+                            <?php $field = get_field_object('name'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('name'); ?></td>
+                            </tr>
+                        <?php endif; ?>
                     </table><!-- /.cell -->
                     <table class="cell">
-                        <tr>
-                            <th>年齢</th>
-                        </tr>
-                        <tr>
-                            <td>21才</td>
-                        </tr>
+                        <?php if (get_field('older')) : ?>
+                            <?php $field = get_field_object('older'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('older'); ?></td>
+                            </tr>
+                        <?php endif; ?>
                     </table><!-- /.cell -->
                     <table class="cell">
-                        <tr>
-                            <th>肩書</th>
-                        </tr>
-                        <tr>
-                            <td>酒やけガール</td>
-                        </tr>
+                        <?php if (get_field('position')) : ?>
+                            <?php $field = get_field_object('position'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('position'); ?></td>
+                            </tr>
+                        <?php endif; ?>
                     </table><!-- /.cell -->
                     <table class="cell">
-                        <tr>
-                            <th>前職</th>
-                        </tr>
-                        <tr>
-                            <td>水商売</td>
-                        </tr>
+                        <?php if (get_field('job')) : ?>
+                            <?php $field = get_field_object('job'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('job'); ?></td>
+                            </tr>
+                        <?php endif; ?>
                     </table><!-- /.cell -->
                     <table class="cell">
-                        <tr>
-                            <th>メッセージ</th>
-                        </tr>
-                        <tr>
-                            <td>飲んで歌ってわちゃわちゃするのが好きです！</td>
-                        </tr>
+                        <?php if (get_field('message')) : ?>
+                            <?php $field = get_field_object('message'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('message'); ?></td>
+                            </tr>
+                        <?php endif; ?>
                     </table><!-- /.cell -->
                 </div><!-- /.cast__table -->
                 <div class="SPACER--100 SP--NONE"></div><!-- /.SPACER--200 -->
-                <div class="cast__button btn-large"><a href="/page-contact.html">オンラインガールズバーの予約へ</a></div><!-- /.cast__button btn-large -->
+                <div class="cast__button btn-large"><a href="<?php echo esc_url(home_url('/contact')); ?>">オンラインガールズバーの予約へ</a></div><!-- /.cast__button btn-large -->
                 <div class="SPACER--200"></div><!-- /.SPACER-200 -->
             </div><!-- /.cast -->
         </div><!-- /.cast-single__inner -->
     </section><!-- /.cast-single -->
-    
-    <footer class="footer">
-        <div class="footer__inner inner">
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-            <div class="footer__menu">
-                <ul class="footer__nav">
-                    <li><a href="">トップページ</a></li>
-                    <li><a href="">お知らせ</a></li>
-                    <li><a href="">キャスト</a></li>
-                    <li><a href="">店舗情報</a></li>
-                    <li><a href="">オンラインガールズバー</a></li>
-                    <li><a href="">ログイン</a></li>
-                </ul><!-- /.footer__nav -->
-            </div><!-- /.footer__menu -->
-            <p class="copyright">
-                Copyright &copy; 株式会社アシスト All Right Reserved
-            </p><!-- /.copyright -->
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-        </div><!-- /.footer__inner inner -->
-    </footer><!-- /.footer -->
+    <?php get_footer(); ?>
 
 </body>
+
 </html>
