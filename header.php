69b2ff789304480aa62a09cf9f7c9b8560ee842b
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-04 21:40:46 +0900

    header.phpの作成とファイル分割

---
+++ header.php
@@ -0,0 +1,13 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php bloginfo('name'); ?></title>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/wow.min.js"></script>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/animate.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+<script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+<?php wp_head(); ?>
