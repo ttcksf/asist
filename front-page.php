acae1b06517e0d680f327d871a275e8c35d54d68
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-05 02:01:43 +0900

    front-page.phpの追加とHTMLのコピペ

---
+++ front-page.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lady merry</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="/js/wow.min.js"></script>
+    <link rel="stylesheet" href="/css/animate.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/script.js"></script>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner inner">
+            <h1 class="header__logo"><a href="/">Lady merry</a></h1>
+            <!-- <ul class="header__nav">
+                <li><a href="" class="is-active">トップページ</a></li>
+                <li><a href="">お知らせ</a></li>
+                <li><a href="">キャスト一覧</a></li>
+                <li><a href="">店内写真</a></li>
+                <li><a href="">オンラインガールズバー</a></li>
+                <li><a href="">ログイン</a></li>
+            </ul> -->
+            <div class="drawer">
+                <input type="checkbox" class="drawer__checkbox" id="drawerCheckbox">
+                <label for="drawerCheckbox" class="drawer__icon">
+                    <span class="drawer__icon-parts"></span><!-- /.drawer__icon-parts -->
+                </label><!-- /.drawer__icon -->
+                <label for="drawerChechbox" class="drawer__overlay"></label><!-- /.drawer__overlay -->
+                <nav class="drawer__menu">
+                    <ul>
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">トップページ</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">お知らせ</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">キャスト</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">店舗情報</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">オンラインガールズバー</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">ログイン</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                    </ul>
+                </nav><!-- /.drawer__menu -->
+            </div><!-- /.drawer -->
+        </div>
+    </header><!-- /.header -->
+
+    <div class="main-visual wow fadeIn" data-wow-duration="4s">
+        <div class="main-visual__inner">
+            <div class="main-visual__left">
+                <div class="main-visual__image">
+                </div><!-- /.main-visual__image -->
+            </div><!-- /.main-visual__left -->
+            <div class="main-visual__right">
+                <div class="main-visual__text">
+                    <h1 class="main-visual__title">Party Night</h1><!-- /.main-visual__title -->
+                    <p class="main-visual__description">I have everything to make your night fun</p>
+                    <!-- /.main-visual__description -->
+                </div><!-- /.main-visual__text -->
+                <div class="SPACER--130 TAB--NONE SP--NONE"></div><!-- /.SPACER--130 -->
+                <div class="main-visual__news">
+                    <h2 class="main-visual__news__head">News & Events</h2><!-- /.main-visual__news__head -->
+                    <div class="main-visual__news__body">
+                        <div class="main-visual__news__item">
+                            <a href="">キャスト、スタッフ積極的に募集中</a>
+                        </div><!-- /.main-visual__news__item -->
+                        <div class="main-visual__news__item">
+                            <a href="">オンラインガールズバー実施中</a>
+                        </div><!-- /.news__item -->
+                        <div class="main-visual__news__item">
+                            <a href="">公式ホームページを開設しました</a>
+                        </div><!-- /.main-visual__news__item -->
+                    </div><!-- /.main-visual__news__body -->
+                </div><!-- /.main-visual__news -->
+            </div><!-- /.main-visual__right -->
+        </div><!-- /.main-visual__inner inner -->
+        <div class="SPACER--130"></div><!-- /.SPACER--130 -->
+    </div><!-- /.main-visual -->
+
+    <section class="online-service wow fadeIn" data-wow-duration="4s">
+        <div class="online-service__inner inner">
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <h1 class="online-service__head">Online Girls Bar</h1><!-- /.online-service__head -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <div class="online-service__all">
+                <div class="online-service__all3">
+                    <div class="online-service__image"></div><!-- /.online-service__image -->
+                    <div class="online-service__content wow fadeInUpBig" data-wow-duration="2s">
+                        <div class="online-service__wrapper">
+                            <h2 class="online-service__title">About Service</h2><!-- /.online-service__title -->
+                            <p class="online-service__description">
+                                当店からの新サービスとして「オンラインガールズバー」を開始致しました。<br>
+                                予約制のため待ち時間や指名のバッティングがありません。<br>
+                                ご自宅にいながらキャストと楽しいお時間を提供します。
+                            </p><!-- /.online-service__description -->
+                            <div class="online-service__btn btn-transparent">
+                                <a href="/">ご予約へ</a>
+                            </div><!-- /.online-service__btn btn -->
+                        </div><!-- /.online-service__wrapper -->
+                    </div><!-- /.online-service__content -->
+                </div><!-- /.online-service__all3 -->
+            </div><!-- /.online-service__all -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+        </div><!-- /.online-service__inner -->
+    </section><!-- /.online-service -->
+
+    <section class="card wow fadeIn" data-wow-duration="4s">
+        <div class="card__inner inner">
+            <div class="SPACER--200"></div><!-- /.SPACER--200 -->
+            <div class="card__items top">
+                <div class="card__item-wrapper wow fadeInUpBig" data-wow-duration="1.5s">
+                    <div class="card__item card__item-menu">
+                        <div class="card__body">
+                            <p class="card__title">Casts</p><!-- /.card__title -->
+                            <p class="card__lead">ノリの良い女の子が<br>そろっています</p><!-- /.card__lead -->
+                            <div class="card__button btn-transparent btn-transparent-vw">
+                                <a href="">詳細へ</a>
+                            </div><!-- /.card__button btn-transparent -->
+                        </div><!-- /.card__body -->
+                    </div><!-- /.card__item -->
+                </div><!-- /.card__item-wrapper -->
+                <div class="card__item-wrapper wow fadeInUpBig" data-wow-duration="2.0s">
+                    <div class="card__item">
+                        <div class="card__image">
+                            <img src="/img/menu-shops.png" alt="">
+                        </div><!-- /.card__image -->
+                    </div><!-- /.card__item -->
+                </div><!-- /.card__item-wrapper -->
+                <div class="card__item-wrapper wow fadeInUpBig" data-wow-duration="2.5s">
+                    <div class="card__item card__item-menu">
+                        <div class="card__body">
+                            <p class="card__title">Recruit</p><!-- /.card__title -->
+                            <p class="card__lead">キャスト、スタッフともに<br>積極的に募集しています</p><!-- /.card__lead -->
+                            <div class="card__button btn-transparent btn-transparent-vw">
+                                <a href="">詳細へ</a>
+                            </div><!-- /.card__button btn-transparent -->
+                        </div><!-- /.card__body -->
+                    </div><!-- /.card__item -->
+                </div><!-- /.card__item-wrapper -->
+            </div><!-- /.card__items -->
+            <div class="card__items under">
+                <div class="card__item-wrapper wow fadeInUpBig" data-wow-duration="3.0s">
+                    <div class="card__item">
+                        <div class="card__image">
+                            <img src="/img/menu-casts.png" alt="">
+                        </div><!-- /.card__image -->
+                    </div><!-- /.card__item -->
+                </div><!-- /.card__item-wrapper -->
+                <div class="card__item-wrapper wow fadeInUpBig" data-wow-duration="3.5s">
+                    <div class="card__item card__item-menu">
+                        <div class="card__body">
+                            <p class="card__title">Shops</p><!-- /.card__title -->
+                            <p class="card__lead">浜松駅からすぐ<br>朝5時まで営業中</p><!-- /.card__lead -->
+                            <div class="card__button btn-transparent btn-transparent-vw">
+                                <a href="">詳細へ</a>
+                            </div><!-- /.card__button btn-transparent -->
+                        </div><!-- /.card__body -->
+                    </div><!-- /.card__item -->
+                </div><!-- /.card__item-wrapper -->
+                <div class="card__item-wrapper wow fadeInUpBig" data-wow-duration="4.0s">
+                    <div class="card__item">
+                        <div class="card__image">
+                            <img src="/img/blog3.png" alt="">
+                        </div><!-- /.card__image -->
+                    </div><!-- /.card__item -->
+                </div><!-- /.card__item-wrapper -->
+            </div><!-- /.card__items under -->
+            <div class="SPACER--200"></div><!-- /.SPACER--200 -->
+        </div><!-- /.card__inner inner -->
+    </section><!-- /.card -->
+
+    <section class="price wow fadeIn" data-wow-duration="4s">
+        <div class="price__inner inner">
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <h1 class="price__head">System</h1><!-- /.price__head -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <div class="price__table wow fadeInUp">
+                <table class="table">
+                    <tr>
+                        <th>1セット(50分)</th>
+                        <td>5,000円</td>
+                    </tr>
+                    <tr>
+                        <th>延長(50分)</th>
+                        <td>5,000円</td>
+                    </tr>
+                    <tr>
+                        <th>Tax・サービス料</th>
+                        <td>15% (※カード支払いの場合は小計から25%)</td>
+                    </tr>
+                </table><!-- /.table -->
+            </div><!-- /.price__table -->
+            <div class="SPACER--100"></div><!-- /.SPACER--100 -->
+        </div><!-- /.price__inner inner -->
+    </section><!-- /.price -->
+
+    <section class="access wow fadeIn" data-wow-duration="4s">
+        <div class="access__inner inner">
+            <div class="SPACER--200 SP--NONE"></div><!-- /.SPACER--200 -->
+            <div class="access__all">
+                <div class="access__all3">
+                    <div class="access__image"></div><!-- /.access-service__image -->
+                    <div class="access__content wow fadeInUp" data-wow-duration="2s">
+                        <div class="access__wrapper">
+                            <h2 class="access__title">Access</h2><!-- /.access-service__title -->
+                            <p class="access__description">
+                                住所：静岡県浜松市中央区<br>
+                                肴町322-20<br>
+                                電話：053-452-3491<br>
+                                営業時間：21時～Last<br>
+                                定休日：水曜日
+                            </p><!-- /.online-service__description -->
+                            <div class="access__btn btn-transparent">
+                                <a href="/">店舗情報へ</a>
+                            </div><!-- /.access__btn btn -->
+                        </div><!-- /.access__wrapper -->
+                    </div><!-- /.access__content -->
+                </div><!-- /.access__all3 -->
+            </div><!-- /.access__all -->
+            <div class="SPACER-100"></div><!-- /.SPACER-100 -->
+        </div><!-- /.access__inner inner -->
+    </section><!-- /.access -->
+    <footer class="footer">
+        <div class="footer__inner inner">
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <div class="footer__menu">
+                <ul class="footer__nav">
+                    <li><a href="">トップページ</a></li>
+                    <li><a href="">お知らせ</a></li>
+                    <li><a href="">キャスト</a></li>
+                    <li><a href="">店舗情報</a></li>
+                    <li><a href="">オンラインガールズバー</a></li>
+                    <li><a href="">ログイン</a></li>
+                </ul><!-- /.footer__nav -->
+            </div><!-- /.footer__menu -->
+            <p class="copyright">
+                Copyright &copy; 株式会社アシスト All Right Reserved
+            </p><!-- /.copyright -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+        </div><!-- /.footer__inner inner -->
+    </footer><!-- /.footer -->
+
+</body>
+
+</html>
