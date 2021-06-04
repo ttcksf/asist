e75a5159a18ec67389783bd88222e686fc407209
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-05 02:57:25 +0900

    archive-cast.phpの作成

--- archive-cast.php
+++ archive-cast.php
@@ -2,157 +2,61 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lady merry</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
-        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="shortcut icon" href="/img/favicon.ico">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="/js/wow.min.js"></script>
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/script.js"></script>
+    <?php get_header(); ?>
 </head>
 
 <body>
-    <header class="header">
-        <div class="header__inner inner">
-            <h1 class="header__logo"><a href="/">Lady merry</a></h1>
-            <!-- <ul class="header__nav">
-                <li><a href="" class="is-active">トップページ</a></li>
-                <li><a href="">お知らせ</a></li>
-                <li><a href="">キャスト一覧</a></li>
-                <li><a href="">店内写真</a></li>
-                <li><a href="">オンラインガールズバー</a></li>
-                <li><a href="">ログイン</a></li>
-            </ul> -->
-            <div class="drawer">
-                <input type="checkbox" class="drawer__checkbox" id="drawerCheckbox">
-                <label for="drawerCheckbox" class="drawer__icon">
-                    <span class="drawer__icon-parts"></span><!-- /.drawer__icon-parts -->
-                </label><!-- /.drawer__icon -->
-                <label for="drawerChechbox" class="drawer__overlay"></label><!-- /.drawer__overlay -->
-                <nav class="drawer__menu">
-                    <ul>
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">トップページ</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">お知らせ</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">キャスト</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">店舗情報</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">オンラインガールズバー</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">ログイン</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                    </ul>
-                </nav><!-- /.drawer__menu -->
-            </div><!-- /.drawer -->
-        </div>
-    </header><!-- /.header -->
+    <?php get_template_part('includes/header-menu'); ?>
     <section class="casts-page">
         <div class="casts-page__inner inner wow fadeIn" data-wow-duration="4s">
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
             <div class="casts__head">Casts</div><!-- /.casts__head -->
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
             <div class="casts__photos">
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast1.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast2.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast3.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast4.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast5.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast6.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast7.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast8.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
-                <a href="/single-cast.html" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
-                    <div class="casts__photo">
-                        <img src="/img/cast9.png" alt="">
-                    </div><!-- /.casts__photo -->
-                </a><!-- /.casts__photo-link -->
+                <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <a href="<?php the_permalink(); ?>" class="casts__photo-link  wow fadeInUpBig" data-wow-duration="1.5s">
+                            <div class="casts__photo">
+                                <?php if (has_post_thumbnail()) :
+                                    $id = get_post_thumbnail_id();
+                                    $img = wp_get_attachment_image_src($id, 'large');
+                                else :
+                                    $img = array(get_template_directory_uri() . '/img/noimg.png');
+                                endif;
+                                ?>
+                                <?php echo '<img src="' . esc_url($img[0]) . '">'; ?>
+                            </div><!-- /.casts__photo -->
+                        </a><!-- /.casts__photo-link -->
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <p>キャストが登録されていません</p>
+                <?php endif; ?>
             </div><!-- /.casts__photos -->
             <div class="SPACER--80"></div><!-- /.SPACER--80 -->
-                <!-- pagenation -->
-                <div class="pagenation-wrapper">
+            <!-- pagenation -->
+            <div class="pagenation-wrapper">
+                <?php if (paginate_links()) : ?>
                     <div class="pagenation">
-                        <span class="page-numbers current">1</span>
-                        <a class="page-numbers" href="#">2</a>
-                        <a class="page-numbers" href="#">3</a>
-                        <a class="page-numbers" href="#">4</a>
-                        <a class="page-numbers" href="#">5</a>
-                        <a class="page-numbers" href="#">6</a>
-                        <a class="page-numbers" href="#">7</a>
-                        <a class="page-numbers" href="#">8</a>
-                        <a class="next page-numbers" href="#"><i class="fas fa-angle-right"></i></a>
+                        <?php
+                        echo paginate_links(array(
+                            'end_size' => 1,
+                            'mid_size' => 1,
+                            'prev_next' => true,
+                            'prev_text' => '<i class="fas fa-angle-left"></i>',
+                            'next_text' => '<i class="fas fa-angle-right"></i>',
+                        ));
+                        ?>
                     </div><!-- /pagenation -->
-                </div><!-- /.pagenation-wrapper -->
+                <?php endif; ?>
+            </div><!-- /.pagenation-wrapper -->
             <div class="SPACER--100"></div><!-- /.SPACER--100 -->
-            <div class="casts-page__button btn-large"><a href="/page-contact.html">オンラインガールズバーの予約へ</a></div><!-- /.card__button btn -->
+            <div class="casts-page__button btn-large"><a href="<?php echo esc_url(home_url('/contact')); ?>">オンラインガールズバーの予約へ</a></div><!-- /.card__button btn -->
             <div class="SPACER--200"></div><!-- /.SPACER--200 -->
         </div><!-- /.casts-page__inner inner -->
     </section><!-- /.casts-page -->
 
-    <footer class="footer">
-        <div class="footer__inner inner">
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-            <div class="footer__menu">
-                <ul class="footer__nav">
-                    <li><a href="">トップページ</a></li>
-                    <li><a href="">お知らせ</a></li>
-                    <li><a href="">キャスト</a></li>
-                    <li><a href="">店舗情報</a></li>
-                    <li><a href="">オンラインガールズバー</a></li>
-                    <li><a href="">ログイン</a></li>
-                </ul><!-- /.footer__nav -->
-            </div><!-- /.footer__menu -->
-            <p class="copyright">
-                Copyright &copy; 株式会社アシスト All Right Reserved
-            </p><!-- /.copyright -->
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-        </div><!-- /.footer__inner inner -->
-    </footer><!-- /.footer -->
+    <?php get_footer(); ?>
 
 </body>
+
 </html>
