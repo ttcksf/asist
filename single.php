0f5ce333e8bc834605c80346f3ea4f484f2ba59d
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-05 02:46:00 +0900

    single.phpの追加とHTMLのコピペ

---
+++ single.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lady merry</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="/js/wow.min.js"></script>
+    <link rel="stylesheet" href="/css/animate.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/script.js"></script>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner inner">
+            <h1 class="header__logo"><a href="/">Lady merry</a></h1>
+            <!-- <ul class="header__nav">
+                <li><a href="" class="is-active">トップページ</a></li>
+                <li><a href="">お知らせ</a></li>
+                <li><a href="">キャスト一覧</a></li>
+                <li><a href="">店内写真</a></li>
+                <li><a href="">オンラインガールズバー</a></li>
+                <li><a href="">ログイン</a></li>
+            </ul> -->
+            <div class="drawer">
+                <input type="checkbox" class="drawer__checkbox" id="drawerCheckbox">
+                <label for="drawerCheckbox" class="drawer__icon">
+                    <span class="drawer__icon-parts"></span><!-- /.drawer__icon-parts -->
+                </label><!-- /.drawer__icon -->
+                <label for="drawerChechbox" class="drawer__overlay"></label><!-- /.drawer__overlay -->
+                <nav class="drawer__menu">
+                    <ul>
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">トップページ</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">お知らせ</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">キャスト</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">店舗情報</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">オンラインガールズバー</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                        <li class="drawer__item"><a href="/" class="drawer__item-inner">ログイン</a>
+                            <!-- /.drawer__item-inner -->
+                        </li><!-- /.drawer__item -->
+                    </ul>
+                </nav><!-- /.drawer__menu -->
+            </div><!-- /.drawer -->
+        </div>
+    </header><!-- /.header -->
+    <section class="news__single">
+        <div class="news__single__inner inner wow fadeIn" data-wow-duration="4s">
+            <div class="single">
+                <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+                <h1 class="single__head">オンラインガールズバーが開始</h1><!-- /.single__head -->
+                <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+                <div class="single__img">
+                    <img src="/img/blog2.png" alt="">
+                </div><!-- /.single__img -->
+                <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+                <div class="single__text">
+                    Lady merryにて新サービスのオンラインガールズバーを開始しました。<br>
+
+                    オンラインガールズバーとは、お客様とキャストがビデオ通話にて食事や会話を楽しんでいただくサービスです。<br>
+
+                    ビデオ通話のため、基本的には同じキャストとずっと会話することができ、途中でヘルプが来たり指名が被ることがありません。<br>
+
+                    サービスの流れは以下の形です。<br>
+
+                    ①本サイトのメニューから「オンラインガールズバー」のページに遷移してください。<br>
+
+                    ②「オンラインガールズバー」のページ内で、ご希望の日程やお客様情報を入力して送信ください。<br>
+
+                    ③Eメールにてクレカによる事前決済を行います。<br>
+
+                    ④決済が完了しましたら、ZoomのURLをご連絡しますので予約時間になりましたら入室ください。<br>  
+                    
+                    ※当店ではZoomを使用しており、Zoomのインストールはあらかじめお客様のほうでお済ませください。<br>
+                </div><!-- /.single__text -->
+                <div class="SPACER--100 SP--NONE"></div><!-- /.SPACER--200 -->
+                <div class="single__button btn-color btn-color-green"><a href="/archive.html">一覧へ</a></div><!-- /.card__button btn -->
+                <div class="SPACER--200"></div><!-- /.SPACER--200 -->    
+            </div><!-- /.single -->    
+            </div><!-- /.news__single__inner -->
+    </section><!-- /.news__single -->
+
+    <section class="cta">
+        <div class="cta__inner inner">
+            <div class="SPACER--160"></div><!-- /.SPACER--160 -->
+            <h1 class="cta-title">キャスト、スタッフ募集中</h1><!-- /.cta-title -->
+            <p class="cta-description">
+                浜松でガールズバーと言えばLady merry。<br>
+                ノルマ、ペナルティは一切ないので未経験から安心して働けます。<br>
+                とにかく仲良しな女の子と一緒に楽しみながら稼ぎませんか？！
+            </p><!-- /.cta-description -->
+            <div class="cta__button btn-color btn-color-blue">
+                <a href="">詳しくはこちら</a>
+            </div><!-- /.cta__button btn-color-blue -->
+            <div class="SPACER--160"></div><!-- /.SPACER--160 -->
+        </div><!-- /.cta__inner -->
+    </section><!-- /.cta -->
+    <footer class="footer">
+        <div class="footer__inner inner">
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+            <div class="footer__menu">
+                <ul class="footer__nav">
+                    <li><a href="">トップページ</a></li>
+                    <li><a href="">お知らせ</a></li>
+                    <li><a href="">キャスト</a></li>
+                    <li><a href="">店舗情報</a></li>
+                    <li><a href="">オンラインガールズバー</a></li>
+                    <li><a href="">ログイン</a></li>
+                </ul><!-- /.footer__nav -->
+            </div><!-- /.footer__menu -->
+            <p class="copyright">
+                Copyright &copy; 株式会社アシスト All Right Reserved
+            </p><!-- /.copyright -->
+            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+        </div><!-- /.footer__inner inner -->
+    </footer><!-- /.footer -->
+
+</body>
+
+</html>
