90468b0dde2fc7b7fdcc873d89dd085db28eedc7
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-04 21:43:12 +0900

    footer.phpの作成とファイル分割

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+    <div class="footer__inner inner">
+        <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+        <div class="footer__menu">
+            <ul class="footer__nav">
+                <li><a href="">トップページ</a></li>
+                <li><a href="">お知らせ</a></li>
+                <li><a href="">キャスト</a></li>
+                <li><a href="">店舗情報</a></li>
+                <li><a href="">オンラインガールズバー</a></li>
+                <li><a href="">ログイン</a></li>
+            </ul><!-- /.footer__nav -->
+        </div><!-- /.footer__menu -->
+        <p class="copyright">
+            Copyright &copy; 株式会社アシスト All Right Reserved
+        </p><!-- /.copyright -->
+        <div class="SPACER--60"></div><!-- /.SPACER--60 -->
+    </div><!-- /.footer__inner inner -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+</footer><!-- /.footer -->
