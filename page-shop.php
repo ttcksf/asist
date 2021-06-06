a3926c9148310f30bc74a245256dcf3958a75fb1
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-06 14:21:31 +0900

    page-shop.phpの作成

--- page-shop.php
+++ page-shop.php
@@ -2,65 +2,11 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lady merry</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
-        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="shortcut icon" href="/img/favicon.ico">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="/js/wow.min.js"></script>
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/script.js"></script>
+    <?php get_header(); ?>
 </head>
 
 <body>
-    <header class="header">
-        <div class="header__inner inner">
-            <h1 class="header__logo"><a href="/">Lady merry</a></h1>
-            <!-- <ul class="header__nav">
-                <li><a href="" class="is-active">トップページ</a></li>
-                <li><a href="">お知らせ</a></li>
-                <li><a href="">キャスト一覧</a></li>
-                <li><a href="">店内写真</a></li>
-                <li><a href="">オンラインガールズバー</a></li>
-                <li><a href="">ログイン</a></li>
-            </ul> -->
-            <div class="drawer">
-                <input type="checkbox" class="drawer__checkbox" id="drawerCheckbox">
-                <label for="drawerCheckbox" class="drawer__icon">
-                    <span class="drawer__icon-parts"></span><!-- /.drawer__icon-parts -->
-                </label><!-- /.drawer__icon -->
-                <label for="drawerChechbox" class="drawer__overlay"></label><!-- /.drawer__overlay -->
-                <nav class="drawer__menu">
-                    <ul>
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">トップページ</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">お知らせ</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">キャスト</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">店舗情報</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">オンラインガールズバー</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">ログイン</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                    </ul>
-                </nav><!-- /.drawer__menu -->
-            </div><!-- /.drawer -->
-        </div>
-    </header><!-- /.header -->
+    <?php get_template_part('includes/header-menu'); ?>
     <section class="shop-page">
         <div class="shop-page__inner inner wow fadeIn" data-wow-duration="4s">
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
@@ -69,28 +15,28 @@
             <h2 class="shop-page__title">Shop Photo</h2><!-- /.shop-page__title -->
             <div class="shop__photos">
                 <div class="shop-photo wow fadeInUpBig" data-wow-duration="1.5s">
-                    <img src="/img/shop1.png" alt="">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/img/shop1.png');?>" alt="">
                 </div><!-- /.shop-photo -->
                 <div class="shop-photo wow fadeInUpBig" data-wow-duration="1.5s">
-                    <img src="/img/shop2.png" alt="">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/img/shop2.png');?>" alt="">
                 </div><!-- /.shop-photo -->
                 <div class="shop-photo wow fadeInUpBig" data-wow-duration="1.5s">
-                    <img src="/img/shop3.png" alt="">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/img/shop3.png');?>" alt="">
                 </div><!-- /.shop-photo -->
                 <div class="shop-photo wow fadeInUpBig" data-wow-duration="1.5s">
-                    <img src="/img/shop4.png" alt="">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/img/shop4.png');?>" alt="">
                 </div><!-- /.shop-photo -->
                 <div class="shop-photo wow fadeInUpBig" data-wow-duration="1.5s">
-                    <img src="/img/shop5.png" alt="">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/img/shop5.png');?>" alt="">
                 </div><!-- /.shop-photo -->
                 <div class="shop-photo wow fadeInUpBig" data-wow-duration="1.5s">
-                    <img src="/img/shop6.png" alt="">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/img/shop6.png');?>" alt="">
                 </div><!-- /.shop-photo -->
                 <div class="shop-photo wow fadeInUpBig" data-wow-duration="1.5s">
-                    <img src="/img/shop7.png" alt="">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/img/shop7.png');?>" alt="">
                 </div><!-- /.shop-photo -->
                 <div class="shop-photo wow fadeInUpBig" data-wow-duration="1.5s">
-                    <img src="/img/shop8.png" alt="">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/img/shop8.png');?>" alt="">
                 </div><!-- /.shop-photo -->
             </div><!-- /.shop__photos -->
             <div class="SPACER--200"></div><!-- /.SAPCER--200 -->
@@ -99,7 +45,7 @@
     <section class="recruit">
         <div class="recruit__inner inner">
             <div class="recruit__image">
-                <img src="/img/recruit.png" alt="">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/img/recruit.png');?>" alt="">
             </div><!-- /.recruit__image -->
             <div class="recruit__content">
                 <h1 class="recruit__content-title">Recruit</h1><!-- /.recruit__content-title -->
@@ -109,30 +55,13 @@
                     体験入店からでも大丈夫なので安心してスタートできます！
                 </p><!-- /.recruit__content-description -->
                 <div class="recruit__button btn-color btn-color-blue-sm">
-                    <a href="">募集情報へ</a>
+                    <a href="https://www.pokepara-tainew.jp/_shizuoka/m231/a261/shop5951/" target="_blank">募集情報へ</a>
                 </div><!-- /.recruit__button btn-color btn-color-blue -->
             </div><!-- /.recruit__content -->
         </div><!-- /.recruit__inner inner -->
     </section><!-- /.recruit -->
-    <footer class="footer">
-        <div class="footer__inner inner">
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-            <div class="footer__menu">
-                <ul class="footer__nav">
-                    <li><a href="">トップページ</a></li>
-                    <li><a href="">お知らせ</a></li>
-                    <li><a href="">キャスト</a></li>
-                    <li><a href="">店舗情報</a></li>
-                    <li><a href="">オンラインガールズバー</a></li>
-                    <li><a href="">ログイン</a></li>
-                </ul><!-- /.footer__nav -->
-            </div><!-- /.footer__menu -->
-            <p class="copyright">
-                Copyright &copy; 株式会社アシスト All Right Reserved
-            </p><!-- /.copyright -->
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-        </div><!-- /.footer__inner inner -->
-    </footer><!-- /.footer -->
+    <?php get_footer(); ?>
 
 </body>
+
 </html>
