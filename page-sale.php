57636c2176724a3fb6ea660c89051114231440e4
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-06 16:15:06 +0900

    page-sale.phpの作成

--- page-sale.php
+++ page-sale.php
@@ -2,72 +2,18 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lady merry</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
-        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="shortcut icon" href="/img/favicon.ico">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="/js/wow.min.js"></script>
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/script.js"></script>
+    <?php get_header(); ?>
 </head>
 
 <body>
-    <header class="header">
-        <div class="header__inner inner">
-            <h1 class="header__logo"><a href="/">Lady merry</a></h1>
-            <!-- <ul class="header__nav">
-                <li><a href="" class="is-active">トップページ</a></li>
-                <li><a href="">お知らせ</a></li>
-                <li><a href="">キャスト一覧</a></li>
-                <li><a href="">店内写真</a></li>
-                <li><a href="">オンラインガールズバー</a></li>
-                <li><a href="">ログイン</a></li>
-            </ul> -->
-            <div class="drawer">
-                <input type="checkbox" class="drawer__checkbox" id="drawerCheckbox">
-                <label for="drawerCheckbox" class="drawer__icon">
-                    <span class="drawer__icon-parts"></span><!-- /.drawer__icon-parts -->
-                </label><!-- /.drawer__icon -->
-                <label for="drawerChechbox" class="drawer__overlay"></label><!-- /.drawer__overlay -->
-                <nav class="drawer__menu">
-                    <ul>
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">トップページ</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">お知らせ</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">キャスト</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">店舗情報</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">オンラインガールズバー</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                        <li class="drawer__item"><a href="/" class="drawer__item-inner">ログイン</a>
-                            <!-- /.drawer__item-inner -->
-                        </li><!-- /.drawer__item -->
-                    </ul>
-                </nav><!-- /.drawer__menu -->
-            </div><!-- /.drawer -->
-        </div>
-    </header><!-- /.header -->
+    <?php get_template_part('includes/header-menu'); ?>
     <section class="sale">
         <div class="sale__inner inner wow fadeIn" data-wow-duration="4s">
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
             <h1 class="sale__head">SalesReport</h1><!-- /.sale__head -->
             <div class="SPACER--60"></div><!-- /.SPACER--60 -->
             <div class="sale__form">
-                <form id="js-form" class="sale__form-wrapper" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfMGrpeQT3hp_jJ7l-q33Rd4HLgtgVpFZE9HJHicJP1cEv6sA/formResponse">
+                <form id="js-form" class="sale__form-wrapper" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSe9AU7agVYdPVkR0odhcJlQw1aHc1i6p7ZaPg2U1XMhze11ww/formResponse">
                     <div class="sale__form-title">売上入力フォーム</div><!-- /.sale__form-title -->
                     <div class="sale__form-description">
                         こちらのフォームに必要事項を入力をお願いします。<br>
@@ -76,19 +22,19 @@
                     <dl class="sale__form-dl">
                         <div class="sale__form-row">
                             <dt class="sale__form-label"><label for="sale-name">担当者</label></dt><!-- /.sale__form-label -->
-                            <dd class="sale__form-input"><input id="sale-name" type="text" name="entry.567582715" placeholder="山田太郎" required></dd><!-- /.sale__form-input -->
+                            <dd class="sale__form-input"><input id="sale-name" type="text" name="entry.638442837" placeholder="山田太郎" required></dd><!-- /.sale__form-input -->
                         </div><!-- /.sale__form-row -->
                         <div class="sale__form-row">
                             <dt class="sale__form-label"><label for="sale-customer">お客様名</label></dt><!-- /.sale__form-label -->
-                            <dd class="sale__form-input"><input id="sale-customer" type="text" name="entry.982333296" placeholder="田中健司様" required></dd><!-- /.sale__form-input -->
+                            <dd class="sale__form-input"><input id="sale-customer" type="text" name="entry.170635837" placeholder="田中健司様" required></dd><!-- /.sale__form-input -->
                         </div><!-- /.sale__form-row -->
                         <div class="sale__form-row">
                             <dt class="sale__form-label"><label for="sale-plan">単価</label></dt><!-- /.sale__form-label -->
-                            <dd class="sale__form-input"><input id="sale-plan" type="text" name="entry.1085549720" placeholder="3000   ※半角数字のみで単位は不要" required></dd><!-- /.sale__form-input -->
+                            <dd class="sale__form-input"><input id="sale-plan" type="text" name="entry.1773584587" placeholder="3000   ※半角数字のみで単位は不要" required></dd><!-- /.sale__form-input -->
                         </div><!-- /.sale__form-row -->
                         <div class="sale__form-row">
                             <dt class="sale__form-label"><label for="sale-count">人数</label></dt><!-- /.sale__form-label -->
-                            <dd class="sale__form-input"><input id="sale-count" type="text" name="entry.1515146269" placeholder="2    ※半角数字のみで単位は不要" required></dd><!-- /.sale__form-input -->
+                            <dd class="sale__form-input"><input id="sale-count" type="text" name="entry.1473412241" placeholder="2    ※半角数字のみで単位は不要" required></dd><!-- /.sale__form-input -->
                         </div><!-- /.sale__form-row -->
                     </dl><!-- /.sale__form-dl -->
                     <div class="sale__form-button">
@@ -97,6 +43,7 @@
                 </form><!-- /.sale__form-wrapper -->
                 <div id="js-success" class="contact-message">
                     <p>送信完了しました ページを再読み込みして新しい内容を入力してください</p>
+                    <div class="entry-404-btn"><a class="btn" href="<?php echo esc_url(home_url('/sale')); ?>">売上管理に戻る</a></div>
                 </div><!-- /.contact-message -->
                 <div id="js-error" class="contact-message -error">
                     <p>送信に失敗しました　ページを再読み込みして再度やり直してください</p>
@@ -105,25 +52,7 @@
             <div class="SPACER--100"></div><!-- /.SPACER--100 -->
         </div><!-- /.sale__inner inner -->
     </section><!-- /.sale -->
-    <footer class="footer">
-        <div class="footer__inner inner">
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-            <div class="footer__menu">
-                <ul class="footer__nav">
-                    <li><a href="">トップページ</a></li>
-                    <li><a href="">お知らせ</a></li>
-                    <li><a href="">キャスト</a></li>
-                    <li><a href="">店舗情報</a></li>
-                    <li><a href="">オンラインガールズバー</a></li>
-                    <li><a href="">ログイン</a></li>
-                </ul><!-- /.footer__nav -->
-            </div><!-- /.footer__menu -->
-            <p class="copyright">
-                Copyright &copy; 株式会社アシスト All Right Reserved
-            </p><!-- /.copyright -->
-            <div class="SPACER--60"></div><!-- /.SPACER--60 -->
-        </div><!-- /.footer__inner inner -->
-    </footer><!-- /.footer -->
-
+    <?php get_footer(); ?>
 </body>
+
 </html>
