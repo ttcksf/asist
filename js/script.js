72892a2f0514983bf7d1431867914c37d3183cfe
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-06 16:14:41 +0900

    非同期通信の処理

--- js/script.js
+++ js/script.js
@@ -8,7 +8,30 @@ jQuery(function ($) {
         $(".main-visual__left").height(windowHeight);
     });
 });
+//googleform
+jQuery(function($){
+    let $form = $('#js-form');
+    $form.submit(function(e){
+        $.ajax({
+            url: $form.attr('action'),
+            data: $form.serialize(),
+            type: "POST",
+            dataType: "xml",
+            statusCode: {
+                0: function(){
+                    $form.slideUp();
+                    $('#js-success').addClass('-active');
+                },
+                200: function(){
+                    $form.slideUp();
+                    $('#js-error').addClass('-active');
+                }
+            }
+        });
+        return false;
+    });
 
+});
 //wow
 jQuery(function($){
     new WOW().init();
