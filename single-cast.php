306cea7974fd11dee1c812618f3502102cbf6ac7
github ttcksf <ttc0104ksf1993@yahoo.co.jp>
2021-06-08 00:43:09 +0900

    カスタムフィールドの入力欄を増大

--- single-cast.php
+++ single-cast.php
@@ -37,6 +37,17 @@
                         <?php endif; ?>
                     </table><!-- /.cell -->
                     <table class="cell">
+                        <?php if (get_field('position')) : ?>
+                            <?php $field = get_field_object('position'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('position'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
                         <?php if (get_field('older')) : ?>
                             <?php $field = get_field_object('older'); ?>
                             <tr>
@@ -48,13 +59,24 @@
                         <?php endif; ?>
                     </table><!-- /.cell -->
                     <table class="cell">
-                        <?php if (get_field('position')) : ?>
-                            <?php $field = get_field_object('position'); ?>
+                        <?php if (get_field('style')) : ?>
+                            <?php $field = get_field_object('style'); ?>
                             <tr>
                                 <th><?php echo $field['label']; ?></th>
                             </tr>
                             <tr>
-                                <td><?php the_field('position'); ?></td>
+                                <td><?php the_field('style'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('blood')) : ?>
+                            <?php $field = get_field_object('blood'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('blood'); ?></td>
                             </tr>
                         <?php endif; ?>
                     </table><!-- /.cell -->
@@ -70,6 +92,39 @@
                         <?php endif; ?>
                     </table><!-- /.cell -->
                     <table class="cell">
+                        <?php if (get_field('from')) : ?>
+                            <?php $field = get_field_object('from'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('from'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('birthday')) : ?>
+                            <?php $field = get_field_object('birthday'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('birthday'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('tobaco')) : ?>
+                            <?php $field = get_field_object('tobaco'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('tobaco'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
                         <?php if (get_field('message')) : ?>
                             <?php $field = get_field_object('message'); ?>
                             <tr>
@@ -77,6 +132,193 @@
                             </tr>
                             <tr>
                                 <td><?php the_field('message'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('kind')) : ?>
+                            <?php $field = get_field_object('kind'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('kind'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('food')) : ?>
+                            <?php $field = get_field_object('food'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('food'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('point')) : ?>
+                            <?php $field = get_field_object('point'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('point'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('color')) : ?>
+                            <?php $field = get_field_object('color'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('color'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('mens')) : ?>
+                            <?php $field = get_field_object('mens'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('mens'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('movie')) : ?>
+                            <?php $field = get_field_object('movie'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('movie'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('place')) : ?>
+                            <?php $field = get_field_object('place'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('place'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('book')) : ?>
+                            <?php $field = get_field_object('book'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('book'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('caractor')) : ?>
+                            <?php $field = get_field_object('caractor'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('caractor'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('present')) : ?>
+                            <?php $field = get_field_object('present'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('present'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('holiday')) : ?>
+                            <?php $field = get_field_object('holiday'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('holiday'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('howto')) : ?>
+                            <?php $field = get_field_object('howto'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('howto'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('trip')) : ?>
+                            <?php $field = get_field_object('trip'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('trip'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('drink')) : ?>
+                            <?php $field = get_field_object('drink'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('drink'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('comedian')) : ?>
+                            <?php $field = get_field_object('comedian'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('comedian'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('music')) : ?>
+                            <?php $field = get_field_object('music'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('music'); ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    </table><!-- /.cell -->
+                    <table class="cell">
+                        <?php if (get_field('brand')) : ?>
+                            <?php $field = get_field_object('brand'); ?>
+                            <tr>
+                                <th><?php echo $field['label']; ?></th>
+                            </tr>
+                            <tr>
+                                <td><?php the_field('brand'); ?></td>
                             </tr>
                         <?php endif; ?>
                     </table><!-- /.cell -->
